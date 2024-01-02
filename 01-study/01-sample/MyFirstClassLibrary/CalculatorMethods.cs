5ba6e8807428bf9777d51ed29de9ba0825ee43fb
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-02 14:03:01 -0300

    chore: create a Class Library project in dotnet csharp

---
+++ 01-study/01-sample/MyFirstClassLibrary/CalculatorMethods.cs
@@ -0,0 +1,28 @@
+﻿namespace MyFirstClassLibrary
+{
+    public class CalculatorMethods
+    {
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static float Subtract(float a, float b)
+        {
+            return a - b;
+        }
+
+        public static float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Divide(float a, float b)
+        {
+            if (a == 0)
+                return 0;
+            else
+                return a / b;
+        }
+    }
+}
--- 01-study/01-sample/MyFirstProject/Program.cs
+++ 01-study/01-sample/MyFirstProject/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+var a = 10;
+var b = 2;
+
+// Addition
+var result = MyFirstClassLibrary.CalculatorMethods.Add(a, b);
+Console.WriteLine(a + " + " + b + " = " + result);
+
+// Subtraction
+result = MyFirstClassLibrary.CalculatorMethods.Subtract(a, b);
+Console.WriteLine(a + " - " + b + " = " + result);
+
+// Multiplication
+result = MyFirstClassLibrary.CalculatorMethods.Multiply(a, b);
+Console.WriteLine(a + " * " + b + " = " + result);
+
+// Division
+result = MyFirstClassLibrary.CalculatorMethods.Divide(a, b);
+Console.WriteLine(a + " / " + b + " = " + result);
+
