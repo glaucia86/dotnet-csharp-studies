ee4d8d847d669ab8dc0db3950bf85f0e464f26a4
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-15 14:58:21 -0300

    chore: sample how to use 'package nuGet'

---
+++ 01-study/01-sample/MyFirstProject/EquationValues.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirstProject
+{
+    public class EquationValues
+    {
+        public float XValue { get; set; }
+        public float YValue { get; set; }
+        public float Result { get; set; }
+    }
+}
--- 01-study/01-sample/MyFirstProject/Program.cs
+++ 01-study/01-sample/MyFirstProject/Program.cs
@@ -1,6 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyFirstProject;
+using Newtonsoft.Json;
 
+Console.WriteLine("Hello, World!");
 
 var a = 10;
 var b = 2;
@@ -20,4 +21,22 @@ Console.WriteLine(a + " * " + b + " = " + result);
 // Division
 result = MyFirstClassLibrary.CalculatorMethods.Divide(a, b);
 Console.WriteLine(a + " / " + b + " = " + result);
+
+string json = @"{
+  'XValue': 2,
+  'YValue': 3,
+  'Result': 0
+}";
+
+var m = JsonConvert.DeserializeObject<EquationValues>(json);
+
+    if (m != null)
+    {
+        m.Result = MyFirstClassLibrary.CalculatorMethods.Add(m.XValue, m.YValue);
+        Console.WriteLine(m.XValue + " + " + m.YValue + " = " + m.Result);
+    } 
+    else
+    {
+    Console.WriteLine("Error...: Could not deserialize json");
+    }
 
